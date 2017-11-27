09c2bbfc5750c479af56affb71a177dc7e59a347
omarattia3143 <omarattia3143@gmail.com>
2017-11-27 22:37:19 +0200

    commit 2

--- Gradproject/WindowsFormsApp1/admin_tools.Designer.cs
+++ Gradproject/WindowsFormsApp1/admin_tools.Designer.cs
@@ -169,7 +169,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(473, 356);
+            this.label2.Location = new System.Drawing.Point(511, 241);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 10;
