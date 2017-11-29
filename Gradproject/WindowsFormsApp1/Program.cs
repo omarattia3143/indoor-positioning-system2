4673bd906927844eaa02091a0e72d525cf8234f8
GelGel <omargalal20084@yahoo.com>
2017-11-29 15:35:20 +0200

    database Schema

--- Gradproject/WindowsFormsApp1/Program.cs
+++ Gradproject/WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new admin_tools());
+            Application.Run(new mapForm());
         }
     }
 }
